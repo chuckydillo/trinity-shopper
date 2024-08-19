10dd410f4ceb42b71234689c5cd834e912885338
Charles Mould <charles.mould.cm@gmail.com>
2024-08-19 16:15:40 +0000

    Created working script from tutorial for adding and removing items

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,29 @@
 console.log("connected")
+
+let grocery = document.getElementById('grocery');
+grocery.addEventListener('submit', addItem);
+function addItem(e){
+e.preventDefault();
+let data = this.elements.writeList.value;
+let list = document.querySelector('ol');
+let item = document.createElement('li');
+let text = document.createElement('p');
+
+text.textContent = data;
+this.elements.writeList.value = "";
+item.append(text);
+list.append(item);
+
+let rmvBtn = document.createElement('span');
+rmvBtn.classList.add('remove');
+item.append(rmvBtn);
+rmvBtn.addEventListener('click' , deleteItem);
+}
+
+function deleteItem(e){
+    this.parentElement.remove();  
+}
+
+function recipeCompare() {
+
+}
